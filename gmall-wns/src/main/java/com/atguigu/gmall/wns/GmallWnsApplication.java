475b6ee3e613dd0570fc0dac8fa0571c366922c8
jointzip <1336134195@qq.com>
2020-07-20 19:13:09 +0800

    谷粒商城环境搭建

---
+++ gmall-wns/src/main/java/com/atguigu/gmall/wns/GmallWnsApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.wns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallWnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmallWnsApplication.class, args);
+	}
+
+}
